9753746e3724c7d341388623e5c8ab7675a0cf05
Richard Sawanaka <richard.s@shakewell.agency>
2025-12-30 21:25:44 +0800

    Add remaining Rails application files (bin, lib, public, test, Docker setup)

---
+++ test/mailers/previews/ticket_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/ticket_mailer
+class TicketMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/ticket_mailer/ticket_confirmation
+  def ticket_confirmation
+    TicketMailer.ticket_confirmation
+  end
+
+end
